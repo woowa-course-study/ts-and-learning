8e2ee9ec0a03a6c393b378c31ad5ad1c7976566b
yunajoe <yunajoe@gmail.com>
2026-04-27 20:29:08 +0900

    exercise-13

--- exercises/exercise-13/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-13/module-augmentations/date-wizard/index.ts
@@ -1,6 +1,13 @@
 // This enables module augmentation mode.
-import 'date-wizard';
+import "date-wizard";
 
-declare module 'date-wizard' {
-    // Add your module extensions here.
+declare module "date-wizard" {
+  // Add your module extensions here.
+  const pad: (ident: number) => string;
+
+  interface DateDetails {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
 }
